b7ab1306c017cd0ead6c32a438c8e259a7fe0a74
SedaMelkhi <gudieva-seda@mail.ru>
2026-01-27 17:22:16 +0300

    добавила библиотеку urfave/cli

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"context"
+	"log"
 
-func main(){
-	fmt.Println("Hello from Hexlet!")
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	app := &cli.Command{
+		Name: "hexlet-path-size",
+		Usage: "print size of a file or directory",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+	}
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+	
 }
